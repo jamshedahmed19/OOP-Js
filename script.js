a7fb92d31f59171762f2c55a67b43da201f0f205
Jamshed Ahmed <jamsheda4ahmed786@gmail.com>
2020-08-13 01:11:50 +0500

    Class Inheritance

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Classes
-class User {
+class Users {
   constructor(name, email) {
     this.name = name;
     this.email = email;
@@ -20,8 +20,8 @@ class User {
   }
 }
 
-let userOne = new User("Jamshed", "jamsheda4ahmed786@gmail.com");
-let userTwo = new User("Hammad", "hammadwasseem@gmail.com");
+let userOne = new Users("Uzair", "uzairahmed786@gmail.com");
+let userTwo = new Users("Hammad", "hammadwasseem@gmail.com");
 
 console.log(userOne, userTwo);
 userTwo.login();
@@ -33,3 +33,33 @@ console.clear();
 console.log("Method Chaining");
 userOne.login().UpdateScore().logout().UpdateScore();
 userTwo.login().UpdateScore().logout().UpdateScore();
+
+// Class Inheritance
+
+// for example we have a website that have user which use
+// Users Class but we also have admins which have access to more
+// methods like delete users but also have same properties and methods
+// as normal users.
+
+// One way is to creating Admin class from scratch we can extend
+// user class
+
+console.clear();
+class Admin extends Users {
+  // we don't need contructor method because it already have
+  // access to Users class contructor method
+  deleteUser(user) {
+    users = users.filter((u) => {
+      return u.email != user.email;
+    });
+  }
+}
+
+let admin = new Admin("Jamshed", "jamsheda4ahmed786@gmail.com");
+let users = [userOne, userTwo, admin];
+// admin deleting a user
+admin.deleteUser(userOne);
+console.log(users);
+// but normal users can't use deleteUser method
+userTwo.deleteUser(userOne);
+console.log(users);
