47051e4d058dfc3c718597fe4eee75e2bb544325
Jamshed Ahmed <jamsheda4ahmed786@gmail.com>
2020-08-13 14:51:42 +0500

    Prototypes

--- script.js
+++ script.js
@@ -1,65 +1,37 @@
 //Classes
-class Users {
-  constructor(name, email) {
-    this.name = name;
-    this.email = email;
-    this.score = 0;
-  }
-  UpdateScore() {
-    this.score++;
-    console.log(this.name, "score is", this.score);
-    return this;
-  }
-  login() {
-    console.log(this.email, "is logged in");
-    return this;
-  }
-  logout() {
-    console.log(this.email, "is logged in");
-    return this;
-  }
+// Js does'nt have Classes itself they were added later
+// before that we have to create objects using proptypes
+// since when we create classes using class keyword we
+// use constructor function which we still have to use in prototypes model
+
+function User(name, email) {
+  this.name = name;
+  this.email = email;
+  this.online = false;
+  // this.login = function () {
+  //   console.log(this.email, "is logged in");
+  // };
 }
+// Now instead of directly defining methods inside construtor function
+// we are using proptype property
+User.prototype.login = function () {
+  this.online = true;
+  console.log(this.email, "is logged in");
+};
+User.prototype.logout = function () {
+  this.online = false;
+  console.log(this.email, "is logged out");
+};
 
-let userOne = new Users("Uzair", "uzairahmed786@gmail.com");
-let userTwo = new Users("Hammad", "hammadwasseem@gmail.com");
+let userOne = new User("Jamshed Ahmed", "jamsheda4ahmed786@gmail.com");
+let userTwo = new User("Hammad Waseem", "hammadwaseem@gmail.com");
 
-console.log(userOne, userTwo);
-userTwo.login();
+// userOne.login();
+// previously when we console log an object methods were shown under
+// the __proto__ property but now they're showing outside of it
+// because we have defined methods directly inside user constructor function
+console.log(userOne);
+userOne.login();
+console.log(userOne);
 userOne.logout();
-console.clear();
-//Method chaining
-//we can not chain methods of classes if it doen't return anything
-//So in order to chain methods we have to return this keyword
-console.log("Method Chaining");
-userOne.login().UpdateScore().logout().UpdateScore();
-userTwo.login().UpdateScore().logout().UpdateScore();
-
-// Class Inheritance
-
-// for example we have a website that have user which use
-// Users Class but we also have admins which have access to more
-// methods like delete users but also have same properties and methods
-// as normal users.
-
-// One way is to creating Admin class from scratch we can extend
-// user class
-
-console.clear();
-class Admin extends Users {
-  // we don't need contructor method because it already have
-  // access to Users class contructor method
-  deleteUser(user) {
-    users = users.filter((u) => {
-      return u.email != user.email;
-    });
-  }
-}
-
-let admin = new Admin("Jamshed", "jamsheda4ahmed786@gmail.com");
-let users = [userOne, userTwo, admin];
-// admin deleting a user
-admin.deleteUser(userOne);
-console.log(users);
-// but normal users can't use deleteUser method
-userTwo.deleteUser(userOne);
-console.log(users);
+console.log(userOne);
