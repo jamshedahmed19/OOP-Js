f731a53f98c351d5a89187006fe1d446a4a75ac9
Jamshed Ahmed <jamsheda4ahmed786@gmail.com>
2020-08-13 15:20:59 +0500

    Prototype Inheritance

--- script.js
+++ script.js
@@ -23,8 +23,31 @@ User.prototype.logout = function () {
   console.log(this.email, "is logged out");
 };
 
-let userOne = new User("Jamshed Ahmed", "jamsheda4ahmed786@gmail.com");
+// Prototype Inheritance
+// ...args will convert the parameter into an array
+function Admin(...args) {
+  // applying user function to new admin object
+  // this refer to admin object
+  User.apply(this, args);
+  // new property only admin have access to
+  this.role = "Admin";
+  // Now admin has access to properties of user class but not
+  // methods
+}
+// since prototype is just an object we are creating admin
+// prototype from user prototype
+Admin.prototype = Object.create(User.prototype);
+// new method only admin have access to
+Admin.prototype.deleteUser = function (u) {
+  users = users.filter((user) => {
+    return user.email != u.email;
+  });
+};
+
+let admin = new Admin("Jamshed Ahmed", "jamsheda4ahmed786@gmail.com");
+let userOne = new User("Uzair Ahmed", "uzairahmed786@gmail.com");
 let userTwo = new User("Hammad Waseem", "hammadwaseem@gmail.com");
+let users = [userOne, userTwo, admin];
 
 // userOne.login();
 // previously when we console log an object methods were shown under
@@ -35,3 +58,7 @@ userOne.login();
 console.log(userOne);
 userOne.logout();
 console.log(userOne);
+console.clear();
+console.log(admin);
+admin.deleteUser(userOne);
+console.log(users);
